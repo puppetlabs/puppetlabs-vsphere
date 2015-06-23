1021ae3c78528911e06a9b24e537a5f333d0d558
phongdly <phong@puppetlabs.com>
2015-06-22 17:05:02 -0700

    (CLOUD-352-WIP) Execute integration test against Shallow Gravy

--- integration/pre-suite/1_install_vsphere_module/00_install_vsphere_from_staging_forge.rb
+++ integration/pre-suite/1_install_vsphere_module/00_install_vsphere_from_staging_forge.rb
@@ -7,6 +7,9 @@ step 'Install puppetlabs-vsphere module on master'
 on(master, puppet('module install puppetlabs-vsphere'))
 
 agents.each do |agent|
+  pe_version = on(agent, puppet('-V')).stdout.rstrip.to_f
+  (pe_version < 4.0)? (path= '/opt/puppet/bin/gem') : (path = '/opt/puppetlabs/puppet/bin/gem')
   step 'install rbvmomi and hocon gems'
-  on(agent, '/opt/puppet/bin/gem install rbvmomi hocon')
+  on(agent, "#{path} install nokogiri -- --use-system-libraries")
+  on(agent, "#{path} install rbvmomi hocon")
 end
