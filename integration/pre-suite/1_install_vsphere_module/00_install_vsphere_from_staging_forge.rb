ff187f78959bf35556bcae84cdc305db91977aab
phongdly <phong@puppetlabs.com>
2015-07-07 10:47:40 -0700

    (CLOUD-355) Make rbvmomi and hocon available to JRuby for Puppet Server

--- integration/pre-suite/1_install_vsphere_module/00_install_vsphere_from_staging_forge.rb
+++ integration/pre-suite/1_install_vsphere_module/00_install_vsphere_from_staging_forge.rb
@@ -1,3 +1,5 @@
+require 'master_manipulator'
+
 test_name "QA-1912 - C64678 - Install using 'puppet module install puppetlabs-vsphere' command"
 
 step 'Setting Staging Forge'
@@ -6,6 +8,15 @@ stub_forge_on(master)
 step 'Install puppetlabs-vsphere module on master'
 on(master, puppet('module install puppetlabs-vsphere'))
 
+# Make rbvmomi and hocon available to JRuby for Puppet Server
+pe_master_version = on(master, puppet('-V')).stdout.rstrip.to_f
+(pe_master_version < 4.0)? (puppetServer_path= '/opt/puppet/bin/puppetserver') : (puppetServer_path = '/opt/puppetlabs/server/bin/puppetserver')
+on(master, "#{puppetServer_path} gem install rbvmomi hocon")
+
+step 'Restart Puppet Server'
+restart_puppet_server(master)
+
+# Install rbvmomi and hocon on master and agent nodes:
 agents.each do |agent|
   pe_version = on(agent, puppet('-V')).stdout.rstrip.to_f
   (pe_version < 4.0)? (path= '/opt/puppet/bin/gem') : (path = '/opt/puppetlabs/puppet/bin/gem')
